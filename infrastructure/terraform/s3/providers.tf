9a78c5a910f3980fa4a19992423250ecabffb289
chris <chris@talos.lan>
2023-04-26 18:24:44 -0400

    test endpoint for minio terraform

--- infrastructure/terraform/s3/providers.tf
+++ infrastructure/terraform/s3/providers.tf
@@ -1,6 +1,6 @@
 provider "minio" {
   alias  = "nas"
-  minio_server   = "s3.kokoro.wtf"
+  minio_server   = "minio.kokoro.wtf"
   minio_user     = module.onepassword_item_minio.fields.username
   minio_password = module.onepassword_item_minio.fields.password
 }
