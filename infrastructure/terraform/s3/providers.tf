89fe3aae0ad4f879f10141e8373a3c907b084056
chris <chris@talos.lan>
2023-04-26 19:46:06 -0400

    test fix minio tf

--- infrastructure/terraform/s3/providers.tf
+++ infrastructure/terraform/s3/providers.tf
@@ -1,6 +1,6 @@
 provider "minio" {
   alias  = "nas"
-  minio_server   = "minio.kokoro.wtf"
+  minio_server   = "minio.ctec.run:9000"
   minio_user     = module.onepassword_item_minio.fields.username
   minio_password = module.onepassword_item_minio.fields.password
 }
