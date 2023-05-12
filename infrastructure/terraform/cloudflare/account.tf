89c53d74ace34cbc08494537b18d07f4c820441c
New Example <39525900+cbc02009@users.noreply.github.com>
2023-05-12 16:04:08 -0400

    updates

--- infrastructure/terraform/cloudflare/account.tf
+++ infrastructure/terraform/cloudflare/account.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_account" "cbc02009" {
-  name              = "cbc02009 Account"
+  name              = "cbc02009's account"
   type              = "standard"
   enforce_twofactor = false
 }
