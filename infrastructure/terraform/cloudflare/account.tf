2fa530ff85fd0282c5fb0e607cd1ab72576f9144
chris <chris@talos.lan>
2023-05-13 20:53:08 -0400

    use flux patch instead of kyverno for ingress

--- infrastructure/terraform/cloudflare/account.tf
+++ infrastructure/terraform/cloudflare/account.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_account" "cbc02009" {
-  name              = "cbc02009's account"
+  name              = "cbc02009 account"
   type              = "standard"
   enforce_twofactor = false
 }
