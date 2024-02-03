607587160260ff46391269e4edac8a6c11987814
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-02-03 13:58:24 -0500

    updates to cilium

--- kubernetes/main/talos/apply-clusterconfig.sh
+++ kubernetes/main/talos/apply-clusterconfig.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 10.0.2.10 -e 10.0.2.10 -f ./clusterconfig/cluster-uiharu.ctec.run.yaml
-talosctl apply-config -n 10.0.2.12 -e 10.0.2.12 -f ./clusterconfig/cluster-sakura.ctec.run.yaml
-talosctl apply-config -n 10.0.2.15 -e 10.0.2.15 -f ./clusterconfig/cluster-miri.ctec.run.yaml
+talosctl apply-config -i -n 10.0.2.10 -e 10.0.2.10 -f ./clusterconfig/cluster-uiharu.ctec.run.yaml
+talosctl apply-config -i -n 10.0.2.12 -e 10.0.2.12 -f ./clusterconfig/cluster-sakura.ctec.run.yaml
+talosctl apply-config -i -n 10.0.2.15 -e 10.0.2.15 -f ./clusterconfig/cluster-miri.ctec.run.yaml
