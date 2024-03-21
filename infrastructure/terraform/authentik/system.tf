be41b763979369c83a04f6f7ed682fb2869468f3
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-03-21 14:07:12 -0400

    disable brand

--- infrastructure/terraform/authentik/system.tf
+++ infrastructure/terraform/authentik/system.tf
@@ -2,15 +2,15 @@ data "authentik_certificate_key_pair" "generated" {
   name = "authentik Self-signed Certificate"
 }
 
-resource "authentik_brand" "home" {
-  domain           = data.doppler_secrets.this.map.DOMAIN
-  default          = false
-  branding_title   = "Home"
+# resource "authentik_brand" "home" {
+#   domain           = data.doppler_secrets.this.map.DOMAIN
+#   default          = false
+#   branding_title   = "Home"
 
-  flow_authentication = authentik_flow.authentication.uuid
-  flow_invalidation   = authentik_flow.invalidation.uuid
-  flow_user_settings  = authentik_flow.user-settings.uuid
-}
+#   flow_authentication = authentik_flow.authentication.uuid
+#   flow_invalidation   = authentik_flow.invalidation.uuid
+#   flow_user_settings  = authentik_flow.user-settings.uuid
+# }
 
 resource "authentik_service_connection_kubernetes" "local" {
   name  = "local"
