4f5321e171254d64b8811ea369a785f3512f08f0
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-03-25 14:27:01 -0400

    update dkim for cloudflare

--- infrastructure/terraform/cloudflare/domain_hardware.tf
+++ infrastructure/terraform/cloudflare/domain_hardware.tf
@@ -34,26 +34,40 @@ module "cf_domain_hardware" {
       value    = "mailsec.protonmail.ch"
       type     = "MX"
     },
+    # {
+    #   id      = "protonmail_dkim_1"
+    #   name    = "protonmail._domainkey"
+    #   value   = "protonmail.domainkey.dyhvzqp4p2xqazzrepen5x2ykwsbchouvlgb4rjl7phvtycq7xtwa.domains.proton.ch."
+    #   type    = "CNAME"
+    #   proxied = false
+    # },
+    # {
+    #   id      = "protonmail_dkim_2"
+    #   name    = "protonmail2._domainkey"
+    #   value   = "protonmail2.domainkey.dyhvzqp4p2xqazzrepen5x2ykwsbchouvlgb4rjl7phvtycq7xtwa.domains.proton.ch."
+    #   type    = "CNAME"
+    #   proxied = false
+    # },
+    # {
+    #   id      = "protonmail_dkim_3"
+    #   name    = "protonmail3._domainkey"
+    #   value   = "protonmail3.domainkey.dyhvzqp4p2xqazzrepen5x2ykwsbchouvlgb4rjl7phvtycq7xtwa.domains.proton.ch."
+    #   type    = "CNAME"
+    #   proxied = false
+    # },
     {
-      id      = "protonmail_dkim_1"
-      name    = "protonmail._domainkey"
-      value   = "protonmail.domainkey.dyhvzqp4p2xqazzrepen5x2ykwsbchouvlgb4rjl7phvtycq7xtwa.domains.proton.ch."
-      type    = "CNAME"
-      proxied = false
+      id = "postmark_dkim"
+      name = "20240322033030pm._domainkey"
+      value = "k=rsa;p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCEyywwsPuQrCfQLqN7NPZYsgK+HWXODZUsx4WR/0s2ivZjUVenbEKHCQp8kjZ3xVjDh+FCftx2VruRDAUXcRDdt445+OL5B58+G1Dm+ccx0KsClSLnwvhw880bn39VYPsCZ6JQ1cJ2q52+F/HMZ+lYeOhSKlqjiwwfzqUiJpaVWQIDAQAB"
+      type = "TXT"
+      proxied = "false"
     },
     {
-      id      = "protonmail_dkim_2"
-      name    = "protonmail2._domainkey"
-      value   = "protonmail2.domainkey.dyhvzqp4p2xqazzrepen5x2ykwsbchouvlgb4rjl7phvtycq7xtwa.domains.proton.ch."
-      type    = "CNAME"
-      proxied = false
-    },
-    {
-      id      = "protonmail_dkim_3"
-      name    = "protonmail3._domainkey"
-      value   = "protonmail3.domainkey.dyhvzqp4p2xqazzrepen5x2ykwsbchouvlgb4rjl7phvtycq7xtwa.domains.proton.ch."
-      type    = "CNAME"
-      proxied = false
+      id = "postmark_return_path"
+      name = "pm-bounces"
+      value = "pm.mtasv.net"
+      type = "cname"
+      proxied = "false"
     },
     {
       id    = "protonmail_spf"
