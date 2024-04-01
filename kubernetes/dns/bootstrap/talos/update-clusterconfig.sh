1ab026267ea3016e599112223bb0d1576ddd6d6f
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-04-01 08:57:21 -0400

    change dns subnet because why not make things more complicated

--- kubernetes/dns/bootstrap/talos/update-clusterconfig.sh
+++ kubernetes/dns/bootstrap/talos/update-clusterconfig.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 10.0.7.10 -e 10.0.7.10 -f ./clusterconfig/dns-ivy.ctec.run.yaml
+talosctl apply-config -n 10.0.5.10 -e 10.0.5.10 -f ./clusterconfig/dns-ivy.ctec.run.yaml
