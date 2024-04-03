2e6b6628ae6417a40ac06a1eb38c9d45eec1def6
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-04-02 20:07:52 -0400

    update external dns to use new endpoint

--- kubernetes/main/bootstrap/talos/update-clusterconfig.sh
+++ kubernetes/main/bootstrap/talos/update-clusterconfig.sh
@@ -3,4 +3,4 @@
 # Deploy the configuration to the nodes
 talosctl apply-config -n 10.0.2.10 -e 10.0.2.10 -f ./clusterconfig/main-uiharu.ctec.run.yaml
 talosctl apply-config -n 10.0.2.12 -e 10.0.2.12 -f ./clusterconfig/main-sakura.ctec.run.yaml
-talosctl apply-config -n 10.0.2.15 -e 10.0.2.15 -f ./clusterconfig/main-miri.ctec.run.yaml
+talosctl apply-config -n 10.0.2.11 -e 10.0.2.11 -f ./clusterconfig/main-miri.ctec.run.yaml
