9eb40e9ef693cf83869dd22691090bec834c3449
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-04-03 22:06:29 -0400

    don't use redis for blocky for right now

--- kubernetes/dns/bootstrap/talos/apply-clusterconfig.sh
+++ kubernetes/dns/bootstrap/talos/apply-clusterconfig.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -i -n 10.0.5.10 -e 10.0.5.10 -f ./clusterconfig/dns-ivy.ctec.run.yaml
+talosctl apply-config -i -n 10.0.2.5 -e 10.0.2.5 -f ./clusterconfig/dns-rem.ctec.run.yaml
--- kubernetes/dns/bootstrap/talos/update-clusterconfig.sh
+++ kubernetes/dns/bootstrap/talos/update-clusterconfig.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config --context dns -n 10.0.2.5 -e 10.0.2.5 -f ./clusterconfig/dns-ivy.ctec.run.yaml
+talosctl apply-config --context dns -n 10.0.2.5 -e 10.0.2.5 -f ./clusterconfig/dns-rem.ctec.run.yaml
