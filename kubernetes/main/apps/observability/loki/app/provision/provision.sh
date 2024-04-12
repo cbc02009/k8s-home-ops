cd666b7fda734f23c34e396a2cc8a13366fc177d
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-04-12 07:20:09 -0400

    update loki
    Signed-off-by: cbc02009 <39525900+cbc02009@users.noreply.github.com>

--- kubernetes/main/apps/observability/loki/app/provision/provision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mcli admin user add minio loki $LOKI_S3_PASSWORD
-mcli mb minio/loki
-mcli admin policy add minio loki-private loki-user-policy.json
-mcli admin policy set minio loki-private user=loki
