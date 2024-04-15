5556dcf3c15eb1381994da0c0cfe3df619dc95d2
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-04-14 22:10:39 -0400

    update s3 endpoint for terraform

--- infrastructure/terraform/s3/providers.tf
+++ infrastructure/terraform/s3/providers.tf
@@ -1,6 +1,6 @@
 provider "minio" {
   alias  = "nas"
-  minio_server   = "s3.kokoro.wtf"
+  minio_server   = "s3.cnas.dev"
   minio_user     = data.doppler_secrets.this.map.MINIO_USER
   minio_password = data.doppler_secrets.this.map.MINIO_PASSWORD
   minio_ssl = true
