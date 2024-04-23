816b7d4a2227ac1cf3bcc02e0ae83f8f79ddf7ec
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-04-23 13:17:32 -0400

    even more updates
    Signed-off-by: cbc02009 <39525900+cbc02009@users.noreply.github.com>

--- kubernetes/dns/bootstrap/talos/apply-clusterconfig.sh
+++ kubernetes/dns/bootstrap/talos/apply-clusterconfig.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -i -n 10.0.2.5 -e 10.0.2.5 -f ./clusterconfig/dns-rem.ctec.run.yaml
+talosctl apply-config -i --context dns -n 10.0.2.5 -e 10.0.2.5 -f ./clusterconfig/dns-rem.ctec.run.yaml
+talosctl apply-config -i --context dns -n 10.0.2.6 -e 10.0.2.6 -f ./clusterconfig/dns-ram.ctec.run.yaml
