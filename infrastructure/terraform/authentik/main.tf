d3c545c60242346b3314422c18bbdccee824fdb3
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2024-05-20 10:26:14 -0400

    move home-automation to 1pass

--- infrastructure/terraform/authentik/main.tf
+++ infrastructure/terraform/authentik/main.tf
@@ -9,17 +9,19 @@ terraform {
   required_providers {
     authentik = {
       source  = "goauthentik/authentik"
-      version = "2024.4.1"
-    }
-    doppler = {
-      source = "DopplerHQ/doppler"
-      version = "1.7.1"
+      version = "2024.4.2"
     }
   }
 }
 
+module "secret_authentik" {
+  # Remember to export OP_CONNECT_HOST and OP_CONNECT_TOKEN
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "k8s-home-ops"
+  item   = "authentik"
+}
 
 provider "authentik" {
-  url   = data.doppler_secrets.this.map.ENDPOINT
-  token = data.doppler_secrets.this.map.TF_TOKEN
+  url   = module.secret_authentik.fields["endpoint"]
+  token = module.secret_authentik.fields["tf_token"]
 }
