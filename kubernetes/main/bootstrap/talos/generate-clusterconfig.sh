5870a9d0218a08f3af3e2a104d5aaf7ca4011de7
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2025-02-21 13:43:41 -0500

    move to flux controller

--- kubernetes/main/bootstrap/talos/generate-clusterconfig.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Generate a new config using talhelper
-doppler run -p talenv-main -c prd talhelper genconfig
--- kubernetes/main/bootstrap/talos/update-clusterconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Deploy the configuration to the nodes
-talosctl apply-config -n 10.0.2.10 -e 10.0.2.10 -f ./clusterconfig/main-uiharu.ctec.run.yaml
-talosctl apply-config -n 10.0.2.12 -e 10.0.2.12 -f ./clusterconfig/main-sakura.ctec.run.yaml
-talosctl apply-config -n 10.0.2.11 -e 10.0.2.11 -f ./clusterconfig/main-miri.ctec.run.yaml
