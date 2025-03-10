bbcbf73946962d5c37202b5b0b39bacb36f28b21
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2025-03-10 10:11:18 -0400

    checkpoint 1

--- kubernetes/dns/bootstrap/talos/apply-clusterconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Deploy the configuration to the nodes
-# talosctl apply-config -i --context dns -n 10.0.2.5 -e 10.0.2.5 -f ./clusterconfig/dns-rem.ctec.run.yaml
-# talosctl apply-config -i --context dns -n 10.0.2.6 -e 10.0.2.6 -f ./clusterconfig/dns-ram.ctec.run.yaml
-talosctl apply-config -i --context dns -n 10.0.2.13 -e 10.0.2.13 -f ./clusterconfig/dns-anya.ctec.run.yaml
--- kubernetes/dns/bootstrap/talos/generate-clusterconfig.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Generate a new config using talhelper
-doppler run -p talenv-dns -c prd talhelper genconfig
