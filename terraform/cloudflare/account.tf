f1be981c46018dd54c5e16f661a9d17eadedeaff
cbc02009 <39525900+cbc02009@users.noreply.github.com>
2025-12-27 00:24:53 +0000

    update cloudflare terraform folder structure

--- terraform/cloudflare/account.tf
+++
@@ -1,5 +0,0 @@
-resource "cloudflare_account" "cbc02009" {
-  name              = "cbc02009 account"
-  type              = "standard"
-  enforce_twofactor = false
-}
--- terraform/cloudflare/providers.tf
+++
@@ -1,4 +0,0 @@
-provider "cloudflare" {
-  email   = module.secret_cf.fields.email
-  api_key = module.secret_cf.fields.api_key
-}
